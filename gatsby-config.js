14c814eab0ed449126c6a1e3fa5b9e71b4ff7a59
lulu70 <liorco70@yahoo.com>
2021-01-30 08:48:34 +0100

    added gatsby-plugin-sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,7 @@ module.exports = {
     image: "/image-name.jpg",
   },
   plugins: [
+    `gatsby-plugin-sass`,
     `gatsby-plugin-advanced-sitemap`,
     // {
     //   resolve: `gatsby-plugin-canonical-urls`,
--- src/components/reusables/SiteWrapper.jsx
+++ src/components/reusables/SiteWrapper.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import "../../../src/utils/custom-bootstrap.css"
+import "../../../src/utils/custom-bootstrap.scss"
 import ErrorBoundary from "./ErrorBoundary"
 import ContextProvider from "../../context/ContextProvider"
 const SiteWrapper = ({ children }) => {
