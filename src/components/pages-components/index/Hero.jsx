d8f1d821c076cfaa48e57d21ed7e5c4892b463be
lulu70 <liorco70@yahoo.com>
2021-03-13 10:19:13 +0100

    .

--- src/components/pages-components/index/Hero.jsx
+++ src/components/pages-components/index/Hero.jsx
@@ -2,15 +2,15 @@ import React from "react"
 import SocialLinks from "../../reusables/SocialLinks"
 
 const Hero = ({ data }) => {
-  const { greetings, title, subtitle } = data.hero.childMdx.frontmatter
+  const { greetings, title, subtitle, links } = data.hero.childMdx.frontmatter
   return (
     <section id="hero" className="mt-40">
       <h1 className="text-5xl">
         {greetings}
         <span className="block mt-6">{title}</span>
       </h1>
-      <h2 className="text-3xl mt-3">{subtitle}</h2>
-      <SocialLinks />
+      <h2 className="text-3xl mt-2">{subtitle}</h2>
+      <SocialLinks links={links} />
     </section>
   )
 }
--- src/components/reusables/SocialLinks.jsx
+++ src/components/reusables/SocialLinks.jsx
@@ -1,22 +1,37 @@
 import React from "react"
+import FacebookIcon from "./FacebookIcon"
+import GithubIcon from "./GithubIcon"
 
-const SocialLinks = () => {
+const SocialLinks = ({ links: linksProp }) => {
   const links = [
     {
       id: 0,
-      name: "Github",
+      name: "github",
+      displayName: "Github",
       url: "https://github.com/lulu70",
+      icon: GithubIcon,
     },
     {
       id: 1,
-      name: "Facebook",
+      name: "facebook",
+      displayName: "Facebook",
       url: "https://www.facebook.com/lior2",
+      icon: FacebookIcon,
     },
-  ]
+  ].filter(link => linksProp.includes(link.name))
   return (
-    <div>
-      {links.map(({ id, name, url }) => (
-        <div key={id}>{name}</div>
+    <div className="inline-flex space-x-5 mt-8">
+      {links.map(({ id, displayName, url, icon: Icon }) => (
+        <a
+          key={id}
+          href={url}
+          target="_blank"
+          rel="noreferrer"
+          className="bg-gray-100 border-2 border-black px-3 py-1 rounded-full w-36 text-center flex justify-between"
+        >
+          <span>{displayName}</span>
+          {Icon && <Icon className="w-5" />}
+        </a>
       ))}
     </div>
   )
