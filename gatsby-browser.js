fb7000f79069e19920f44a6827aabb3669f57b64
lulu70 <liorco70@yahoo.com>
2021-04-08 18:03:13 +0200

    added Browser is not supported for IE

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,24 @@
 import React from "react"
 import SiteWrapper from "./src/components/reusables/SiteWrapper"
+import isBrowser from "./src/helpers/isBrowser"
 
-export const wrapRootElement = ({ element }) => (
-  <SiteWrapper>{element}</SiteWrapper>
-)
+export const wrapRootElement = ({ element }) => {
+  // Display Browser is not supported for IE
+  if (isBrowser()) {
+    if (window.document.documentMode) {
+      return (
+        <h1
+          style={{
+            color: "white",
+            textAlign: "center",
+            fontSize: "5rem",
+          }}
+        >
+          This Browser is not supported
+        </h1>
+      )
+    }
+  }
+
+  return <SiteWrapper>{element}</SiteWrapper>
+}
