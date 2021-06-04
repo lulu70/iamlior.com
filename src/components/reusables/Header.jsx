f5ecd69a20ccb6936d70d5ec8c66a4e7aff862c9
lulu70 <liorco70@yahoo.com>
2021-06-04 09:43:01 +0200

    added ScrollProgressBar

--- src/components/reusables/Header.jsx
+++ src/components/reusables/Header.jsx
@@ -5,23 +5,27 @@ import useTabIsUsed from "../../hooks/useTabIsUsed"
 import DarkModeSwitch from "./DarkModeSwitch"
 import SiteIcon from "./SiteIcon"
 import getClassNamesByTabIsUsedState from "../../helpers/getClassNamesByTabIsUsedState"
+import ScrollProgressBar from "./ScrollProgressBar"
 
 const Header = () => {
   const tabIsUsed = useTabIsUsed()
 
   return (
-    <header className="z-50 flex py-2 items-center sticky top-0 bg-lightTheme-bg dark:bg-darkTheme-bg">
-      <Link
-        to="/"
-        aria-label="brand"
-        className={`flex text-3xl lg:text-4xl ${getClassNamesByTabIsUsedState(
-          tabIsUsed
-        )}`}
-      >
-        <SiteIcon className="w-20 h-full fill-current" />
-      </Link>
-      <NavigationMenu />
-      <DarkModeSwitch tabIsUsed={tabIsUsed} />
+    <header className="z-50 sticky top-0 bg-lightTheme-bg dark:bg-darkTheme-bg flex flex-col">
+      <div className="flex items-center py-2">
+        <Link
+          to="/"
+          aria-label="brand"
+          className={`flex text-3xl lg:text-4xl ${getClassNamesByTabIsUsedState(
+            tabIsUsed
+          )}`}
+        >
+          <SiteIcon className="w-20 h-full fill-current" />
+        </Link>
+        <NavigationMenu />
+        <DarkModeSwitch tabIsUsed={tabIsUsed} />
+      </div>
+      <ScrollProgressBar />
     </header>
   )
 }
---
+++ src/components/reusables/ScrollProgressBar.jsx
@@ -0,0 +1,39 @@
+import React from "react"
+import styled from "styled-components"
+import tw from "twin.macro"
+import { gsap } from "gsap"
+import ScrollTrigger from "gsap/ScrollTrigger"
+gsap.registerPlugin(ScrollTrigger)
+
+const Progress = styled.progress`
+  /* For Chrome or Safari */
+  ::-webkit-progress-bar {
+    ${tw`dark:bg-darkTheme-primary bg-lightTheme-primary`};
+  }
+
+  ::-webkit-progress-value {
+    background-color: ${tw`bg-accent`} !important;
+  }
+
+  /* For Firefox */
+  ${tw`dark:bg-darkTheme-primary bg-lightTheme-primary`}
+  ::-moz-progress-bar {
+    background-color: ${tw`bg-accent`} !important;
+  }
+`
+const ScrollProgressBar = () => {
+  const progressRef = React.useRef()
+  React.useEffect(() => {
+    gsap.to(progressRef.current, {
+      value: 100,
+      scrollTrigger: {
+        scrub: 0.3,
+      },
+    })
+  }, [])
+  return (
+    <Progress ref={progressRef} value={0} max={100} className="w-full h-1" />
+  )
+}
+
+export default ScrollProgressBar
