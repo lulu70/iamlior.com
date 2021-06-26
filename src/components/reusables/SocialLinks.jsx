1b5fea08e08820205cc0c07670e10b1495af9e73
lulu70 <liorco70@yahoo.com>
2021-06-26 16:11:47 +0200

    changed ersume link to google drive

--- src/components/reusables/SocialLinks.jsx
+++ src/components/reusables/SocialLinks.jsx
@@ -72,7 +72,7 @@ const SocialLinks = ({ className }) => {
       name: "resume",
       displayName: "Resume",
       url:
-        "https://firebasestorage.googleapis.com/v0/b/iamlior.appspot.com/o/Lior%20Cohen%20Graphic%20Resume.pdf?alt=media&token=dbc019ea-631d-400e-96a1-f6660379cebf",
+        "https://drive.google.com/file/d/1oHZ9MynQh_UWQz3_PCpEze-P5uEG4jwx/view?usp=sharing",
       icon: DocumentIcon,
     },
   ]
