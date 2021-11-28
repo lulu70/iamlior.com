9567f3f93298a9ff85b57e612e18ddc8ad695722
lulu70 <liorco70@yahoo.com>
2021-11-28 10:40:33 +0100

    removed the covid project

--- src/components/pages-components/index/MyWork.tsx
+++ src/components/pages-components/index/MyWork.tsx
@@ -17,11 +17,14 @@ const MyWork = ({
 }) => {
   const title = data?.childMdx?.frontmatter?.title
   const { tabIsUsed } = React.useContext(MainContext)
+  const visibleProjects = projects.nodes.filter(
+    ({ childMdx }) => childMdx?.frontmatter?.visible
+  )
   return (
     <section id="my-work" className="">
       <SectionHeader>{title}</SectionHeader>
       <div id="projects-container" className="mt-4 space-y-12">
-        {projects.nodes.map((project, index) => {
+        {visibleProjects.map((project, index) => {
           const id = project.id
           const body = project.childMdx?.body
           const frontmatter = project.childMdx?.frontmatter
@@ -31,78 +34,75 @@ const MyWork = ({
           const screenshot = getImage(
             frontmatter?.screenshot?.childImageSharp?.gatsbyImageData
           )
-          const visible = frontmatter?.visible
           const external = frontmatter?.external ?? ""
           const tags = frontmatter?.tags ?? []
           return (
-            visible && (
-              <div
-                id="project-container"
-                key={id}
-                className={`bg-lightTheme-primary dark:bg-darkTheme-primary p-8 rounded-3xl space-y-4 max-w-xl ${
-                  index % 2 !== 0 ? "ml-auto" : "mr-auto"
-                }`}
-              >
-                <div className="max-w-xl">
-                  <span id="emoji" className="mr-2">
-                    {emoji}
-                  </span>
-                  <span id="category">{category}</span>
-                  <h4 id="title" className="text-xl font-normal mt-4">
-                    {title}
-                  </h4>
-                  {body && <MDXRenderer>{body}</MDXRenderer>}
-                </div>
-                <a
-                  id={"link to" + title}
-                  className={`flex relative overflow-hidden ${getClassNamesByTabIsUsedState(
-                    tabIsUsed
-                  )}`}
-                  href={external}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {screenshot && (
-                    <GatsbyImage
-                      image={screenshot}
-                      alt={title}
-                      className="rounded-lg"
-                    />
-                  )}
-                  <Pointer />
-                </a>
-                <div id="tags-container" className="flex flex-wrap max-w-xl">
-                  {tags.map(tag => (
-                    <UnderlineText key={tag}>{tag}</UnderlineText>
-                  ))}
-                </div>
-                <a
-                  id={"external link to" + title}
-                  className={`inline-block ${getClassNamesByTabIsUsedState(
-                    tabIsUsed
-                  )}`}
-                  href={external}
-                  target="_blank"
-                  rel="noreferrer"
-                  aria-label={title}
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    className="text-gray-400 w-8 h-8"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
-                    />
-                  </svg>
-                </a>
+            <div
+              id="project-container"
+              key={id}
+              className={`bg-lightTheme-primary dark:bg-darkTheme-primary p-8 rounded-3xl space-y-4 max-w-xl ${
+                index % 2 !== 0 ? "ml-auto" : "mr-auto"
+              }`}
+            >
+              <div className="max-w-xl">
+                <span id="emoji" className="mr-2">
+                  {emoji}
+                </span>
+                <span id="category">{category}</span>
+                <h4 id="title" className="text-xl font-normal mt-4">
+                  {title}
+                </h4>
+                {body && <MDXRenderer>{body}</MDXRenderer>}
               </div>
-            )
+              <a
+                id={"link to" + title}
+                className={`flex relative overflow-hidden ${getClassNamesByTabIsUsedState(
+                  tabIsUsed
+                )}`}
+                href={external}
+                target="_blank"
+                rel="noreferrer"
+              >
+                {screenshot && (
+                  <GatsbyImage
+                    image={screenshot}
+                    alt={title}
+                    className="rounded-lg"
+                  />
+                )}
+                <Pointer />
+              </a>
+              <div id="tags-container" className="flex flex-wrap max-w-xl">
+                {tags.map(tag => (
+                  <UnderlineText key={tag}>{tag}</UnderlineText>
+                ))}
+              </div>
+              <a
+                id={"external link to" + title}
+                className={`inline-block ${getClassNamesByTabIsUsedState(
+                  tabIsUsed
+                )}`}
+                href={external}
+                target="_blank"
+                rel="noreferrer"
+                aria-label={title}
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  className="text-gray-400 w-8 h-8"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+                  />
+                </svg>
+              </a>
+            </div>
           )
         })}
       </div>
